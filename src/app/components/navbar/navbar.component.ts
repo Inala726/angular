474e1f386fb7fdc7a99d4329d3dda08229cf7799
Inala <inalajacob740@gmail.com>
2025-04-16 05:54:25 +0100

    done w responsiveness(I think??)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {ionMenu} from '@ng-icons/ionicons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 
 @Component({
+  imports: [NgIcon],
+  viewProviders: [provideIcons({ionMenu})],
   selector: 'app-navbar',
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  isMenuOpen: boolean = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }

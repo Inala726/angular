7e990dc5e5b7605a831774cee341b86873d21287
Inala <inalajacob740@gmail.com>
2025-04-16 13:02:43 +0100

    included objectives

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import {bootstrapHouseDoor, bootstrapShieldCheck, bootstrapLightningCharge} from '@ng-icons/bootstrap-icons';
+import {bootstrapHouseDoor, bootstrapShieldCheck, bootstrapLightningCharge, bootstrapPerson, bootstrapUiRadiosGrid, bootstrapLaptop} from '@ng-icons/bootstrap-icons';
 import { FooterComponent } from '../components/footer/footer.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 
@@ -8,7 +8,7 @@ import { NavbarComponent } from '../components/navbar/navbar.component';
 @Component({
   selector: 'app-homepage',
   imports: [NgIcon, FooterComponent, NavbarComponent],
-  viewProviders: [provideIcons({bootstrapHouseDoor, bootstrapShieldCheck, bootstrapLightningCharge})],
+  viewProviders: [provideIcons({bootstrapHouseDoor, bootstrapShieldCheck, bootstrapLightningCharge, bootstrapPerson, bootstrapUiRadiosGrid, bootstrapLaptop})],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })

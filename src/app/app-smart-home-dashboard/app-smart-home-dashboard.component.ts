6b87f2d8f37748272477593ff9d5626ad32637c7
Inala <inalajacob740@gmail.com>
2025-06-20 21:02:46 +0100

    skii

--- src/app/app-smart-home-dashboard/app-smart-home-dashboard.component.ts
+++ src/app/app-smart-home-dashboard/app-smart-home-dashboard.component.ts
@@ -1,18 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { bootstrapBell, bootstrapGear, bootstrapHouse, bootstrapPlus } from '@ng-icons/bootstrap-icons';
-import { heroHome } from '@ng-icons/heroicons/outline';
+import { 
+  bootstrapBell, 
+  bootstrapGear, 
+  bootstrapHouse, 
+  bootstrapPlus,
+  bootstrapThermometerHalf 
+} from '@ng-icons/bootstrap-icons';
+import { heroHome, heroLightBulb } from '@ng-icons/heroicons/outline';
 import { monoLogOut } from '@ng-icons/mono-icons';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { DevicesService } from '../services/devices.service';
-import { DeviceRegistrationModalComponent } from '../components/device-registration-modal/device-registration-modal.component';
+// import { DeviceRegistrationModalComponent } from '../components/device-registration-modal/device-registration-modal.component';
 import { AuthService } from '../services/auth.service';
+import { DevicesService } from '../services/devices.service';
 
 @Component({
   selector: 'smart-home-dashboard',
   standalone: true,
-  imports: [CommonModule, NgIcon, DeviceRegistrationModalComponent],
-  viewProviders: [provideIcons({ bootstrapHouse, bootstrapGear, bootstrapPlus, bootstrapBell, heroHome, monoLogOut })],
+  imports: [CommonModule, NgIcon],
+  viewProviders: [provideIcons({ 
+    bootstrapHouse, 
+    bootstrapGear, 
+    bootstrapPlus, 
+    bootstrapBell, 
+    heroHome, 
+    monoLogOut,
+    heroLightBulb,
+    bootstrapThermometerHalf
+  })],
   templateUrl: './app-smart-home-dashboard.component.html',
   styleUrls: ['./app-smart-home-dashboard.component.scss'],
 })
